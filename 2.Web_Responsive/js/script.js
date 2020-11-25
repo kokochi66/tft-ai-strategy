6372ad3d93038e264cdd2fa3424e7cf084a389c0
kokochi66 <kokochikochi@gmail.com>
2020-11-25 21:33:01 +0900

    2020.11.25.Responsive.nav-open

--- 2.Web_Responsive/js/script.js
+++ 2.Web_Responsive/js/script.js
@@ -17,5 +17,26 @@ $(".slider").slick({
 // 메인 컨텐츠 col4 이미지 슬라이더 효과
 
 
-$(".lightbox").lightGallery();
+$(".lightbox").lightGallery({
+  autoplay: true,
+  pause: 3000,
+  progressBar: true
+});
 // 메인 컨텐츠 col5 라이트 박스 효과
+
+$(".button").click(function(e){
+  e.preventDefault();
+  $(".nav").slideToggle();
+  $(".button").toggleClass("open");
+
+  if( $(".button").hasClass("open") ){
+    $(".button").find("i").attr("class","fa fa-arrow-up");
+  } else {
+    $(".button").find("i").attr("class","fa fa-arrow-down");
+  }
+});
+$(window).resize(function(){
+  var wWidth = $(window).width();
+  if(wWidth >= 600) $(".nav").removeAttr("style");
+})
+// 내비 접기/펼치기
