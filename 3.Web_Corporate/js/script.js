beb843587709413a32efe69036f7415dae700b28
kokochi66 <kokochikochi@gmail.com>
2020-12-06 22:20:54 +0900

    2020.12.06.Corporate.chart-script

--- 3.Web_Corporate/js/script.js
+++ 3.Web_Corporate/js/script.js
@@ -9,3 +9,19 @@ var mySwiper = new Swiper('.swiper-container',{
     delay: 5000,
   },
 });
+// 스와이퍼
+
+
+var movBtn = $(".title > ul li");
+var movContents = $(".chart_cont");
+movContents.hide();
+movContents.eq(0).show();
+movBtn.click(function(e) {
+  e.preventDefault();
+  var target = $(this);
+  var index = target.index();
+  movBtn.removeClass("active");
+  target.addClass("active");
+  movContents.hide();
+  movContents.eq(index).show();
+});
