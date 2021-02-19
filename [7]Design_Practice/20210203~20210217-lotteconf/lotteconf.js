1f5d475d68ab49969422f51f22c1d779a1eafd15
kokochi66 <kokochikochi@gmail.com>
2021-02-19 23:27:02 +0900

    2021.02.19.lotteconf

--- [7]Design_Practice/20210203~20210217-lotteconf/lotteconf.js
+++ [7]Design_Practice/20210203~20210217-lotteconf/lotteconf.js
@@ -94,7 +94,6 @@ document.addEventListener('DOMContentLoaded', () => {
   cont03_swiper.on('transitionEnd', function (e) {
     cont03_active_slide = document.querySelector('.cont03 .swiper-container .swiper-wrapper .swiper-slide-active');
     var data_index = cont03_active_slide.getAttribute('data-swiper-slide-index');
-    console.log(cont03_ban_item[data_index]);
     cont03_text.forEach(elem => {
       elem.classList.remove('active');
     });
@@ -107,21 +106,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
   var cont04 = document.querySelector('#contents .cont04'),
       cont04_slider = document.getElementById('cont04-slider'),
-      cont04_prev = document.querySelector('#contents #cont04-slider .prev'),
-      cont04_next = document.querySelector('#contents #cont04-slider .next');
-
+      cont04_prev = document.querySelector('.cont04 .prev'),
+      cont04_next = document.querySelector('.cont04 .next'),
+      cont04_slides = document.querySelectorAll('.cont04 .swiper-slide');
   var cont04_swiper = new Swiper(cont04_slider, {
       navigation: {   // 버튼 사용자 지정
       nextEl: cont04_next,
       prevEl: cont04_prev,
     },
   });
+  var countTest = document.getElementById('counttest');
+  //카운트 효과
+
+  cont04_swiper.on('transitionEnd', function (e) {
+    var cont04_active_slide = document.querySelector('.cont04 .swiper-slide-active');
+    var counter_text = cont04_active_slide.querySelectorAll('.info_text');
+    counter_text.forEach(elem => {
+      var curr_text = elem.querySelector('strong');
+      var curr_count = curr_text.getAttribute('data-count');
+      var c_counter = new countUp.CountUp(curr_text, curr_count, {
+        decimalPlaces: 1,
+      });
+      c_counter.start();
+    });
+
+  }); // 슬라이드가 움직일 때 마다 텍스트 변경효과
+
+
   document.addEventListener('scroll', function(e){
     if(window.scrollY != 0) {
       header.classList.add('scroll');
     } else header.classList.remove('scroll');
-    if(window.scrollY >= cont01.getBoundingClientRect().top+100) cont01.classList.add('display');
-    if(window.scrollY >= cont02.getBoundingClientRect().top+100) cont02.classList.add('display');
-    if(window.scrollY >= cont03.getBoundingClientRect().top+100) cont03.classList.add('display');
+    if(window.scrollY >= cont01.getBoundingClientRect().top+window.pageYOffset - 500) cont01.classList.add('display');
+    if(window.scrollY >= cont02.getBoundingClientRect().top+window.pageYOffset - 500) cont02.classList.add('display');
+    if(window.scrollY >= cont03.getBoundingClientRect().top+window.pageYOffset - 500) cont03.classList.add('display');
+    if(window.scrollY >= cont04.getBoundingClientRect().top+window.pageYOffset - 500) cont04.classList.add('display');
   }); // 스크롤 이벤트
+  if(window.scrollY != 0) {
+    header.classList.add('scroll');
+  } else header.classList.remove('scroll');
+  if(window.scrollY >= cont01.getBoundingClientRect().top+window.pageYOffset - 500) cont01.classList.add('display');
+  if(window.scrollY >= cont02.getBoundingClientRect().top+window.pageYOffset - 500) cont02.classList.add('display');
+  if(window.scrollY >= cont03.getBoundingClientRect().top+window.pageYOffset - 500) cont03.classList.add('display');
+  if(window.scrollY >= cont04.getBoundingClientRect().top+window.pageYOffset - 500) cont04.classList.add('display');
+  // 스크롤 최초 초기화
 });
