6e7b5f9c9933ca2976360e589e402f629501e8b9
kokochi66 <kokochikochi@gmail.com>
2021-05-03 20:12:37 +0900

    2021.05.03.Lpoint

---
+++ [7]Design_Practice/20210502_Lpoint/Lpoint.js
@@ -0,0 +1,16 @@
+
+
+var wrap = document.getElementById('wrap'),
+    header = document.getElementById('header'),
+    h_title = document.querySelector('#header .title'),
+    h_nav = document.querySelector('#header .nav'),
+    h_util = document.querySelector('#header .utils'),
+    contents = document.getElementById('contents'),
+    footer = document.getElementById('footer');
+const open = setTimeout(function(){
+  header.classList.add('active');
+
+
+
+
+},500);
