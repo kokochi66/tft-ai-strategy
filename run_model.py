ffe69b8dad0e67aee7bce51395663e2b117ba5b7
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2023-07-30 03:46:28 +0900

    데이터 수집 수정, 추가 고려사항 기재

--- run_model.py
+++ run_model.py
@@ -19,16 +19,21 @@ def predict_character(img_path):
     img_tensor = preprocess_image(img_path)
     prediction = loaded_model.predict(img_tensor)
     
-    # 예측 결과 출력
-    predicted_class = np.argmax(prediction, axis=1)
-        # 클래스 정보 불러오기
+    # 클래스 정보 불러오기
     with open('class_indices.json', 'r') as f:
         class_indices = json.load(f)
     
     class_labels = list(class_indices.keys())
+    
+    # 전체 예측 확률 출력
+    for i, label in enumerate(class_labels):
+        print(f"{label}: {prediction[0][i] * 100:.2f}%")
+    
+    # 가장 높은 확률의 클래스 출력
+    predicted_class = np.argmax(prediction, axis=1)
     predicted_label = class_labels[predicted_class[0]]
-    return predicted_label
+    return f"Most likely: {predicted_label}"
 
 # 예측 수행
-img_path = "war.png"
+img_path = "./test/ir.png"
 print(predict_character(img_path))
